ccc4e9b5a0d9e037b5e3af9370a8e4ba1cbbed18
Raviteja-2004 <147155158+Raviteja-2004@users.noreply.github.com>
2024-03-26 13:34:39 +0530

    Delete bird-species-classification-master directory

--- bird-species-classification-master/run_job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source ./venv/bin/activate
-python3 -u ./train_model.py --weight_path=$1 \
-        --history_path=$2 \
-				--lock_file=$3 \
-        --config_file=$4 \
-				--first_epoch=$5
--- bird-species-classification-master/scripts/pp.py
+++
@@ -1,4 +0,0 @@
-from bird import preprocessing as pp
-
-pp.preprocess_data_set("datasets/birdClef2016Subset/valid",
-                    "datasets/birdClef2016Subset_preprocessed/valid")
